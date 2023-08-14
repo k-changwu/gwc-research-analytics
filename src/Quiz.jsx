5fb8f1d6fc9a8bbd9e3fd9aa453abf1d9687e93d
k-changwu <katherinechangwu@example.com>
2023-08-14 14:00:04 -0400

    landing screen added

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./index.css";
 import logo from './images/logo-mint.png';
 
@@ -7,8 +7,9 @@ import logo from './images/logo-mint.png';
 const Quiz = ({questions}) => {
     const[currentQuestion, setCurrentQuestion] = useState(0);
     const {question, choices} = questions[currentQuestion];
-    const [answerIdx, setAnswerIdx] = useState(null);
+    const [answerIdx, setAnswerIdx] = useState(null); /* answerIndx = index of the choice selected */
     const [answer, setAnswer] = useState(null);
+    const [quizStarted, setQuizStarted] = useState(false);
 
     const onAnswerClick = (answer, index) => {
         setAnswerIdx(index);
@@ -18,14 +19,26 @@ const Quiz = ({questions}) => {
         }else{
             setAnswer(false);
     }
+}
+    const onStartQuiz = () => {
+        setQuizStarted(true);
+        setAnswerIdx(null); 
+    };
+    
 
-
-    }
+    
+    useEffect(() => {
+        document.title = "Salesforce or Tableau?"
+    }, []);
 
     return  (
+        <div>
+        {/* <h1>Salesforce or Tableau?</h1> */}
         <div className = "quiz-container" >
             <img src = {logo} alt= "Logo" className="Logo"/>
+            {quizStarted ? (
         <>
+
         <span className = "active-question-no">{currentQuestion + 1}</span>
         <span className = "total-question">/{questions.length}</span>
         <h2>{question} </h2>
@@ -42,10 +55,37 @@ const Quiz = ({questions}) => {
                      </li>
                     
                      
-                ))
-            }
+                ))}
         </ul>
-        </>    
+        <div className = "footer">
+           
+            <button
+                 onClick={() => {
+                    if (currentQuestion === questions.length - 1) {
+                    // Handle finishing the quiz
+                    } else {
+                        setCurrentQuestion(currentQuestion + 1);
+                        setAnswerIdx(null);
+                    }
+                    }}
+                        disabled={answerIdx === null}
+                    >
+            {currentQuestion === questions.length - 1 ? 'Finish' : 'Next'}
+            </button>
+        </div>
+        </>
+        ):(
+            <div className="landing-page">
+                    <h1>Welcome to the Salesforce vs Tableau Survey!</h1>
+                    <p>Take this survey to decide if you should create a Salesforce Report or Tableau Dashboard. </p>
+                    <p>Click the "Start" button to begin.</p>
+                    <button onClick = {onStartQuiz}>Start</button>
+                </div>
+
+            )}
+         
+    </div>
+   
     </div>
     );
     
